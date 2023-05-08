7ec75c7891a570990ca537d01635bb4abaab612e
Khalid Mohammed <khalarasta104@gmail.com>
2023-05-08 22:38:18 +0300

    Initial Commit

---
+++ shell.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * main - Main arguments functions
+ * @ac: Count of arguments
+ * @av: Arguments
+ * @env: Environment
+ * Return: _exit = 0.
+ */
+
+int main(int ac, char **av, char **env)
+{
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+extern char **environ;
+
+#endif /* SHELL_H */

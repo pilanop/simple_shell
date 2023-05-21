c2d350379390a782369019e2a75fdd9fc99452e1
Khalid Mohammed <khalarasta104@gmail.com>
2023-05-21 19:18:58 +0300

    header file

--- shell.h
+++ shell.h
@@ -12,4 +12,32 @@
 
 extern char **environ;
 
+int sh_exit(char **args);
+
+int _strcmp(char *s1, char *s2);
+
+size_t _strncmp(char *s1, char *s2, size_t n);
+
+int _strlen(char *s);
+
+char *_strcpy(char *dest, char *src);
+
+char *_strcat(char *dest, char *src);
+
+int _putchar(char c);
+
+char *_get_path(char **env);
+
+int _values_path(char **arg, char **env);
+
+char *_getline_command(void);
+
+void _getenv(char **env);
+
+char **tokenize(char *lineptr);
+
+void _exit_command(char **args, char *lineptr, int _exit);
+
+int _fork_fun(char **arg, char **av, char **env, char *lineptr, int np, int c);
+
 #endif /* SHELL_H */
